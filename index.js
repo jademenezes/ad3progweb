8eef2a035ce198464fa42fbc3be9ea02abad40b5
Jade Menezes <jaderegis97@gmail.com>
2025-05-28 13:12:52 -0300

    servindo arquivos JSON do servidor

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
+app.get('/sobre', (req, res) => {
+  res.send('Esta será a página sobre');
+});
+
 app.get('/professores', (req, res) => {
+  // carrega o arquivo professores.json
   const professores = require('./public/professores.json');
 
+  // Envia o arquivo JSON como resposta
   res.json({ professores });
 });
 
